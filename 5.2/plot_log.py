214766e131f465b04ebc047febd3d212b9de18fd
Lorinc Balassa <lb3314@ic.ac.uk>
2016-10-12 12:40:02 +0100

    Legends show up in plots

--- 5.2/plot_log.py
+++ 5.2/plot_log.py
@@ -18,6 +18,7 @@ mot0angle = data[:, 2 + 2 * motorid]
 #x = np.linspace(0, 10)
 plt.plot(x, mot0ref, label='Reference angle')
 plt.plot(x, mot0angle, label='Actual angle')
+plt.legend()
 
 plt.title(filename + " motor " + str(motorid))
 
