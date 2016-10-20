aa2bf3d57de2fc38dd7b8a3a4fdcca49464a7896
Lorinc Balassa <lb3314@ic.ac.uk>
2016-10-20 10:26:09 +0000

    many trials were done, still veering left

--- pid_tuning.py
+++ pid_tuning.py
@@ -30,14 +30,14 @@ RIGHTMOTORPARAMS.feedForwardGain = 255/20.0
 RIGHTMOTORPARAMS.pidParameters.minOutput = -255
 RIGHTMOTORPARAMS.pidParameters.maxOutput = 255
 RIGHTMOTORPARAMS.pidParameters.k_p = 444.0
-RIGHTMOTORPARAMS.pidParameters.k_i = 250.0
+RIGHTMOTORPARAMS.pidParameters.k_i = 100.0
 RIGHTMOTORPARAMS.pidParameters.k_d = 60.0
 
 
-#TEST_KP = [i for i in range(600,901,20)]
+TEST_KP = [516,516,516,516,516,546,546,546,546,546]
 #TEST_KD = [i for i in range(5,201,5)]
+#TEST_KI = [i for i in range(75,126,10)]
 #TEST_ANGLE = [i for i in range (10,51,10)]
-TEST_PWM = [i for i in range(40,101,10)]
 folder = "PID_log_" + "-".join("_".join(time.ctime().split(" ")).split(":"))
 os.mkdir(folder)
 angle = 20 #test rotation angle of 20 rads
@@ -46,20 +46,20 @@ interface.setMotorAngleControllerParameters(motors[0],LEFTMOTORPARAMS)
 interface.setMotorAngleControllerParameters(motors[1],RIGHTMOTORPARAMS)
 
 #interface.startLogging(folder + "/KP_TUNE_TEST.log")
-for test_val in TEST_PWM:
+for test_val in TEST_KP:
 	#interface.startLogging("PID_log/KP_800_KD_TUNE_VAL%d.log" %(test_val))
-	interface.startLogging(folder + "/KP_TUNE_PWM_%d.log" % (test_val))
-
-	print "Now testing minPWM of %03d..." %(test_val)
+	interface.startLogging(folder + "/KP_TUNE_%03d.log" % (test_val))
+	
+	print "Now testing k_p of %03d..." %(test_val)
 	#print "Now testing k_d value of %3d" %(test_val)
-	LEFTMOTORPARAMS.minPWM = test_val
-	RIGHTMOTORPARAMS.minPWM = test_val
-	interface.increaseMotorAngleReferences(motors,[20,20])
+	LEFTMOTORPARAMS.pidParameters.k_p = test_val
+	interface.increaseMotorAngleReferences(motors,[60,60])
 
-	#input('press any key to continue')
 	time.sleep(5)	
 
 	interface.stopLogging()
+	temp = raw_input('press any key to continue')
+
 	#interface.increaseMotorAngleReferences([motors[0]],[0])
 #interface.stopLogging()
 #for test_val in TEST_KP:
