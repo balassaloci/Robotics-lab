8cb2c34b69003bb3a3119b3546192ec8c1734496
Lorinc Balassa <lb3314@ic.ac.uk>
2016-10-21 14:51:04 +0000

    "PID_SAturate_test"

--- pid_tuning.py
+++ pid_tuning.py
@@ -11,6 +11,8 @@ speed = 1.0
 interface.motorEnable(motors[0])
 interface.motorEnable(motors[1])
 
+cpid = 0.1
+
 LEFTMOTORPARAMS = interface.MotorAngleControllerParameters()
 LEFTMOTORPARAMS.maxRotationAcceleration = 6.0
 LEFTMOTORPARAMS.minPWM = 18.0
@@ -18,9 +20,9 @@ LEFTMOTORPARAMS.maxRotationSpeed = 12.0
 LEFTMOTORPARAMS.feedForwardGain = 255/20.0
 LEFTMOTORPARAMS.pidParameters.minOutput = -255
 LEFTMOTORPARAMS.pidParameters.maxOutput = 255
-LEFTMOTORPARAMS.pidParameters.k_p = 456.0
-LEFTMOTORPARAMS.pidParameters.k_i = 150.0
-LEFTMOTORPARAMS.pidParameters.K_d = 40.0
+LEFTMOTORPARAMS.pidParameters.k_p = 456.0 * cpid
+LEFTMOTORPARAMS.pidParameters.k_i = 150.0 * cpid
+LEFTMOTORPARAMS.pidParameters.K_d = 40.0 * cpid
 
 RIGHTMOTORPARAMS = interface.MotorAngleControllerParameters()
 RIGHTMOTORPARAMS.maxRotationAcceleration = 6.0
@@ -29,9 +31,11 @@ RIGHTMOTORPARAMS.maxRotationSpeed = 12.0
 RIGHTMOTORPARAMS.feedForwardGain = 255/20.0
 RIGHTMOTORPARAMS.pidParameters.minOutput = -255
 RIGHTMOTORPARAMS.pidParameters.maxOutput = 255
-RIGHTMOTORPARAMS.pidParameters.k_p = 444.0
-RIGHTMOTORPARAMS.pidParameters.k_i = 150.0
-RIGHTMOTORPARAMS.pidParameters.K_d = 40.0
+RIGHTMOTORPARAMS.pidParameters.k_p = 444.0 * cpid
+RIGHTMOTORPARAMS.pidParameters.k_i = 150.0 * cpid
+RIGHTMOTORPARAMS.pidParameters.K_d = 40.0 * cpid
+
+
 
 
 #TEST_KP = [x for x in range(300,901,100)]
@@ -54,7 +58,7 @@ left_ratio = 1.00 + ratio_diff/2
 for test_val in TEST_KP:
 #	interface.startLogging("PID_log/KP_TUNE_VAL%d.log" %(test_val))
 #	interface.startLogging(folder + "/KP_TUNE_%03d.log" % (test_val))
-    interface.startLogging(folder + "/KD_TUNE_40.log")
+    interface.startLogging(folder + "/PID_TUNE_CONSTMULTI.log")
     #print "Now testing k_p of %03d..." %(test_val)
     #print "Now testing k_d value of %3d" %(test_val)
     interface.increaseMotorAngleReferences(motors,[60 * left_ratio,60 * right_ratio])
