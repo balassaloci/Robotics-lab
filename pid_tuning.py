eda7adc8424951f1d91072a68ec8de0e8563897c
Lorinc Balassa <lb3314@ic.ac.uk>
2016-10-21 13:49:58 +0000

    changed left motor, decreased right max speed

--- pid_tuning.py
+++ pid_tuning.py
@@ -18,23 +18,23 @@ LEFTMOTORPARAMS.maxRotationSpeed = 12.0
 LEFTMOTORPARAMS.feedForwardGain = 255/20.0
 LEFTMOTORPARAMS.pidParameters.minOutput = -255
 LEFTMOTORPARAMS.pidParameters.maxOutput = 255
-LEFTMOTORPARAMS.pidParameters.k_p = 456.0
-LEFTMOTORPARAMS.pidParameters.k_i = 250.0
-LEFTMOTORPARAMS.pidParameters.k_d = 60.0
+LEFTMOTORPARAMS.pidParameters.k_p = 100.0
+LEFTMOTORPARAMS.pidParameters.k_i = 0.0
+LEFTMOTORPARAMS.pidParameters.k_d = 0.0
 
 RIGHTMOTORPARAMS = interface.MotorAngleControllerParameters()
 RIGHTMOTORPARAMS.maxRotationAcceleration = 6.0
 RIGHTMOTORPARAMS.minPWM = 18.0
-RIGHTMOTORPARAMS.maxRotationSpeed = 12.0
+RIGHTMOTORPARAMS.maxRotationSpeed = 11.7
 RIGHTMOTORPARAMS.feedForwardGain = 255/20.0
 RIGHTMOTORPARAMS.pidParameters.minOutput = -255
 RIGHTMOTORPARAMS.pidParameters.maxOutput = 255
-RIGHTMOTORPARAMS.pidParameters.k_p = 444.0
-RIGHTMOTORPARAMS.pidParameters.k_i = 100.0
-RIGHTMOTORPARAMS.pidParameters.k_d = 60.0
+RIGHTMOTORPARAMS.pidParameters.k_p = 100.0
+RIGHTMOTORPARAMS.pidParameters.k_i = 0.0
+RIGHTMOTORPARAMS.pidParameters.k_d = 0.0
 
 
-TEST_KP = [456]
+TEST_KP = [x for x in range(100,901,100)]
 #TEST_KD = [i for i in range(5,201,5)]
 #TEST_KI = [i for i in range(75,126,10)]
 #TEST_ANGLE = [i for i in range (10,51,10)]
@@ -47,17 +47,18 @@ interface.setMotorAngleControllerParameters(motors[1],RIGHTMOTORPARAMS)
 
 #interface.startLogging(folder + "/KP_TUNE_TEST.log")
 for test_val in TEST_KP:
-	#interface.startLogging("PID_log/KP_800_KD_TUNE_VAL%d.log" %(test_val))
-#	interface.startLogging(folder + "/KP_TUNE_%03d.log" % (test_val))
+#	interface.startLogging("PID_log/KP_TUNE_VAL%d.log" %(test_val))
+	interface.startLogging(folder + "/KP_TUNE_%03d.log" % (test_val))
 	
 #	print "Now testing k_p of %03d..." %(test_val)
 	#print "Now testing k_d value of %3d" %(test_val)
 	LEFTMOTORPARAMS.pidParameters.k_p = test_val
+	RIGHTMOTORPARAMS.pidParameters.k_p = test_val
 	interface.increaseMotorAngleReferences(motors,[60,60])
 
 	time.sleep(5)	
 
-#	interface.stopLogging()
+	interface.stopLogging()
 	temp = raw_input('press any key to continue')
 
 	#interface.increaseMotorAngleReferences([motors[0]],[0])
