c8e059bd1a8df80f3f900dac017dce2bdc6607f4
Lorinc Balassa <lb3314@ic.ac.uk>
2016-10-21 14:31:11 +0000

    set PID values for new motor

--- pid_tuning.py
+++ pid_tuning.py
@@ -18,9 +18,9 @@ LEFTMOTORPARAMS.maxRotationSpeed = 12.0
 LEFTMOTORPARAMS.feedForwardGain = 255/20.0
 LEFTMOTORPARAMS.pidParameters.minOutput = -255
 LEFTMOTORPARAMS.pidParameters.maxOutput = 255
-LEFTMOTORPARAMS.pidParameters.k_p = 100.0
-LEFTMOTORPARAMS.pidParameters.k_i = 0.0
-LEFTMOTORPARAMS.pidParameters.k_d = 0.0
+LEFTMOTORPARAMS.pidParameters.k_p = 456.0
+LEFTMOTORPARAMS.pidParameters.k_i = 150.0
+LEFTMOTORPARAMS.pidParameters.K_d = 20.0
 
 RIGHTMOTORPARAMS = interface.MotorAngleControllerParameters()
 RIGHTMOTORPARAMS.maxRotationAcceleration = 6.0
@@ -29,12 +29,13 @@ RIGHTMOTORPARAMS.maxRotationSpeed = 12.0
 RIGHTMOTORPARAMS.feedForwardGain = 255/20.0
 RIGHTMOTORPARAMS.pidParameters.minOutput = -255
 RIGHTMOTORPARAMS.pidParameters.maxOutput = 255
-RIGHTMOTORPARAMS.pidParameters.k_p = 100.0
-RIGHTMOTORPARAMS.pidParameters.k_i = 0.0
-RIGHTMOTORPARAMS.pidParameters.k_d = 0.0
+RIGHTMOTORPARAMS.pidParameters.k_p = 444.0
+RIGHTMOTORPARAMS.pidParameters.k_i = 150.0
+RIGHTMOTORPARAMS.pidParameters.K_d = 20.0
 
 
-TEST_KP = [x for x in range(300,901,100)]
+#TEST_KP = [x for x in range(300,901,100)]
+TEST_KP = [100]
 #TEST_KD = [i for i in range(5,201,5)]
 #TEST_KI = [i for i in range(75,126,10)]
 #TEST_ANGLE = [i for i in range (10,51,10)]
@@ -52,20 +53,14 @@ left_ratio = 1.00 + ratio_diff/2
 #interface.startLogging(folder + "/KP_TUNE_TEST.log")
 for test_val in TEST_KP:
 #	interface.startLogging("PID_log/KP_TUNE_VAL%d.log" %(test_val))
-	interface.startLogging(folder + "/KP_TUNE_%03d.log" % (test_val))
-
+#	interface.startLogging(folder + "/KP_TUNE_%03d.log" % (test_val))
+    interface.startLogging(folder + "/KD_TUNE_13.log")
     #print "Now testing k_p of %03d..." %(test_val)
-	#print "Now testing k_d value of %3d" %(test_val)
-
-	LEFTMOTORPARAMS.pidParameters.k_p = test_val
-	RIGHTMOTORPARAMS.pidParameters.k_p = test_val
-	interface.increaseMotorAngleReferences(motors,[60 * left_ratio,60 * right_ratio])
-
-	time.sleep(9)
-
-	interface.stopLogging()
-	temp = raw_input('press any key to continue')
-
+    #print "Now testing k_d value of %3d" %(test_val)
+    interface.increaseMotorAngleReferences(motors,[60 * left_ratio,60 * right_ratio])
+    time.sleep(9)
+    interface.stopLogging()
+    temp = raw_input('press any key to continue')
 	#interface.increaseMotorAngleReferences([motors[0]],[0])
 #interface.stopLogging()
 #for test_val in TEST_KP:
