0349b52e7cb15089ca139a7b87a6d4f5d052f5b0
Lorinc Balassa <lb3314@ic.ac.uk>
2016-10-26 10:56:54 +0100

    Touch sensor initial test code

---
+++ 3_2.1_On_Off/sensor_run.py
@@ -0,0 +1,13 @@
+import robolib
+
+robot = robolib.robolib()
+
+#def do_square():
+#    for _ in range(4):
+#        robot.straight(40)
+#        robot.turn(90)
+#
+#do_square()
+
+robot.sense()
+print("Done with the square")
